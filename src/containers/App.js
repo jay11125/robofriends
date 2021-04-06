24f4a1e91e315b8b9f0ec4fb17fbfe9b8d5e8151
jay11125 <70865848+jay11125@users.noreply.github.com>
2021-04-06 16:14:11 +0530

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,24 +2,25 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import { robots } from "../robots";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: [],
+      robots: robots,
       searchfield: "",
     };
   }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => {
-        this.setState({ robots: users });
-      });
-  }
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => response.json())
+  //     .then((users) => {
+  //       this.setState({ robots: users });
+  //     });
+  // }
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
