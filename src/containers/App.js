9fe624c9586a43dae42a964ed37effa941ceb1f0
jay11125 <70865848+jay11125@users.noreply.github.com>
2021-04-06 15:45:06 +0530

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,8 +28,7 @@ class App extends Component {
     const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    return !robots.length ?
-      <h1>Loading</h1> :
+    return
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
